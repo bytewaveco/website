e2f4dcf0a0d940c88e329af818177e951bd33d5b
Kaden Griffith <kadengriffith@gmail.com>
2025-04-04 11:28:57 -0600

    chore: update dependencies
    chore: remove uneeded config

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,18 +2,7 @@ export default defineAppConfig({
   uiPro: {
     pageLogos: {
       slots: {
-        root: 'relative overflow-hidden',
-        title: 'text-lg text-center font-semibold text-(--ui-text-highlighted)',
         logos: 'mt-0',
-        logo: 'size-10 shrink-0 !justify-start',
-      },
-      variants: {
-        marquee: {
-          false: {
-            logos:
-              'flex items-center shrink-0 gap-(--gap) [--gap:--spacing(16)]',
-          },
-        },
       },
     },
   },
